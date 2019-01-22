90e51c72772d6ebd32e2c377fc5a6de2631f23b4
Anonymous <admin@hda.me>
2019-01-22 14:07:24 +0000

    fix windows builds

--- dist-xp/hx50ripper/compressxp.sh
+++ dist-xp/hx50ripper/compressxp.sh
@@ -1,2 +1,2 @@
-7z a -sfx7zWin32.sfx ./hx50ripper-winxp-x64.exe ./win64
-7z a -sfx7zWin32.sfx ./hx50ripper-winxp-x32.exe ./win32
+zip -9 -r ./hx50ripper-winxp-x32.zip ./win32
+zip -9 -r ./hx50ripper-winxp-x64.zip ./win64
--- dist/hx50ripper/compress.sh
+++ dist/hx50ripper/compress.sh
@@ -1,5 +1,5 @@
 XZ_OPT=-9 tar -Jcvf ./hx50ripper-linux32.tar.xz ./linux32/
 XZ_OPT=-9 tar -Jcvf ./hx50ripper-linux64.tar.xz ./linux64/
 zip -9 -r ./hx50ripper-osx64.zip ./osx64
-7z a -sfx7zWin32.sfx ./hx50ripper-windows-x64.exe ./win64
-7z a -sfx7zWin32.sfx ./hx50ripper-windows-x32.exe ./win32
+zip -9 -r ./hx50ripper-win64.zip ./win64
+zip -9 -r ./hx50ripper-win32.zip ./win32
